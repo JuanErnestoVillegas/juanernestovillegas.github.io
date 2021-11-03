eeedb93ecd88fde59c2c9913dbdc6e39bbf23b1d
JuanErnestoVillegas <91296469+JuanErnestoVillegas@users.noreply.github.com>
2021-11-03 20:44:31 -0300

    Modificado

--- ChallengeNro17/app.js
+++ ChallengeNro17/app.js
@@ -2,7 +2,7 @@ let dni=0;
 let bandera=false;
 
 while(bandera!==true){
-    dni = prompt('Ingrese su DNI.');
+    dni = Number(prompt('Ingrese su DNI.'));
     if((!isNaN(dni)) && dni!=0 &&(dni.length === 7 || dni.length === 8))  
             {
                 bandera=true;
