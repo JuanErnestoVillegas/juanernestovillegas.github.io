6d9961da6234daadfbf99e3f7d30c6b87eacecd2
Juan Ernesto Villegas <jevillegas_123@hotmail.com>
2022-01-04 07:58:56 -0300

    Chall16 deleted

--- app.js
+++
@@ -1,7 +0,0 @@
-let numero=Number(prompt('Ingrese un número para mostrar su tabla de multiplicar'));
-let i;
-
-console.log(`La Tabla del ${numero} es:`);
-for(i=1;i<=10;i++){
-    console.log(`${numero}x${i} = `,numero*i);
-}
