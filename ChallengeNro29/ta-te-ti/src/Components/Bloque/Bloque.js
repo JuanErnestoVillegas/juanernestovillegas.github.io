e055d07867dd3c56ec6f22f54b866ec91214518f
Juan Ernesto Villegas <jevillegas_123@hotmail.com>
2022-03-12 00:38:42 -0300

    Change 2

--- ChallengeNro29/ta-te-ti/src/Components/Bloque/Bloque.js
+++ ChallengeNro29/ta-te-ti/src/Components/Bloque/Bloque.js
@@ -2,7 +2,7 @@ import './Bloque.css'
 
 const Bloque = ({onClick}) => {
     return (
-        <button className='square m-1' onClick={onClick}></button>        
+        <button className='square m-1' style={{ height: '20vh', width: '20vh' }} size="lg" onClick={onClick}></button>        
       );
 }
  
