edf534395d6812e06438a47a9f28a3d57c6e3b00
Juan Ernesto Villegas <jevillegas_123@hotmail.com>
2022-03-20 21:38:29 -0300

    Funcionando

--- ChallengeNro29/ta-te-ti/src/Components/Tablero/Tablero.js
+++ ChallengeNro29/ta-te-ti/src/Components/Tablero/Tablero.js
@@ -1,25 +1,33 @@
 import { useState } from "react";
 import Bloque from './../Bloque/Bloque';
 import './Tablero.css';
+import Swal from "sweetalert2";
 
+function refreshPage() {
+    window.location.reload(false);
+  }
 
 const Tablero = () =>{
     const [player, setPlayer] = useState("X");
     const [game, setGame] = useState(["", "", "", "", "", "", "", "", ""]);
     let status;
-    let finalizada=Boolean; 
-
+    let finalizada=false; 
+    let playerAnt;
+    
     const handlePlay = (position) => {
-        if(game[position]!=='') return;
-        let newState = game;        
-        newState[position] = player;
-        setGame(newState);
-                
-        if(player==='X'){
-            setPlayer('O')
-        }else{
-            setPlayer('X')
-        }        
+            playerAnt = player;
+            if(game[position]!=='') return;
+            let newState = game;        
+            newState[position] = player;
+            console.log(`JUGADOR: ${player}`);
+            if(finalizada==false){
+                setGame(newState);
+                if(player==="X"){
+                    setPlayer("O")
+                }else{
+                    setPlayer("X")
+                }                     
+   }
     };
 
     const handleStatus = () => {
@@ -33,23 +41,31 @@ const Tablero = () =>{
             [0,4,8],
             [2,4,6]
         ];
+
+
         lines.forEach(([x,y,z])=>{
-            if(game[x]==game[y] && game[x]==game[z] && game[x]!=''){
-                status = `Ganó ${player}`;
+            if(game[x]==game[y] && game[x]==game[z] && game[x]!=''){           
                 finalizada=true;
+                playerAnt=game[x];          
+                status = `Ganó ${playerAnt}`;     
+                Swal.fire("Juego terminado", `GANADOR: ${playerAnt}`, "success");  
+                setTimeout(() => {
+                    refreshPage(); 
+                  }, 2000);                                 
             }else{
                 status = `Juega ${player}`;
             }
         }
-    )
-    }
+        )
+        return null;    
+}
+    
 
     handleStatus();
-
     return (
         <>
         <div id="titulo">TA-TE-TI</div>
-        <p>{status}</p>
+        <h3 id="estado">{status}</h3>
         <br />
         <div className="tateti">  
                 <div className="table-row">
@@ -68,6 +84,7 @@ const Tablero = () =>{
                     <Bloque onClick={() => handlePlay(8)} player={game[8]} />           
                 </div>       
             </div> 
+            <button type="submit" onClick={refreshPage}>Limpiar</button>            
         </>
       );
     };
